eac5a51197a48d20f637e09f0632ba14d29430a3
Maciej Malecki <maciej.malecki@capgemini.com>
2021-02-08 22:07:34 +0100

    H2 with TCP/IP access

---
+++ java/mtsj/core/src/main/java/com/devonfw/application/mtsj/general/dataaccess/base/H2Configuration.java
@@ -0,0 +1,15 @@
+package com.devonfw.application.mtsj.general.dataaccess.base;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2Configuration {
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+    }
+}
